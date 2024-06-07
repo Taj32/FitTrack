804ac80042dbd9812dc26ff5121e0f850c57e773
Tajvir Chahal <tajvirchahal87@gmail.com>
2024-06-07 18:21:19 -0400

    6/7 - Graph CSS work

--- frontend/gym-app/app/(tabs)/home.tsx
+++ frontend/gym-app/app/(tabs)/home.tsx
@@ -6,34 +6,39 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { CartesianChart, CartesianChartRenderArg, Line } from "victory-native";
 import { ReactNode } from 'react';
+import { BicepEmoji } from '@/components/BicepEmoji';
+import { useFont } from "@shopify/react-native-skia";
+import inter from '@/assets/fonts/SpaceMono-Regular.ttf'; //frontend/gym-app/assets/fonts/inter-medium.ttf
+//  @/assets/fonts/inter-medium.ttf
 
-const DATA = Array.from({ length: 31 }, (_, i) => ({
+const DATA = Array.from({ length: 30 }, (_, i) => ({
     day: i,
     highTmp: 40 + 30 * Math.random(),
   }));
 
 export default function HomeScreen() {
+    const font = useFont(inter, 8);
     return (
 
         <ParallaxScrollView
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
             headerImage={
                 <Image
-                    source={require('@/assets/images/running-man.png')}
+                    source={require('@/assets/images/running-man.png')} 
                     style={styles.reactLogo}
                 />
             }>
             <ThemedView style={styles.titleContainer}>
                 <ThemedText type="title">Hello, Taj..</ThemedText>
-                <ThemedText type="title">💪</ThemedText>
+                <BicepEmoji/>
             </ThemedView>
 
-            <View style={{ height: 200 }}>
-                <CartesianChart data={DATA} xKey="day" yKeys={["highTmp"]}>
+            <View style={styles.chart}>
+                <CartesianChart data={DATA} xKey="day" yKeys={["highTmp"]} domain={{y:[0,100]}} axisOptions={{font}}  >
                     {/* 👇 render function exposes various data, such as points. */}
                     {({ points }) => (
                     // 👇 and we'll use the Line component to render a line path.
-                    <Line points={points.highTmp} color="red" strokeWidth={3} />
+                    <Line points={points.highTmp} color="red" strokeWidth={3} curveType='natural' animate={{ type: "timing", duration: 300 }} />
                     )}
                 </CartesianChart>
             </View>
@@ -43,7 +48,7 @@ export default function HomeScreen() {
                     {/* 👇 render function exposes various data, such as points. */}
                     {({ points }) => (
                     // 👇 and we'll use the Line component to render a line path.
-                    <Line points={points.highTmp} color="purple" strokeWidth={3} />
+                    <Line points={points.highTmp} color="purple" strokeWidth={3}  />
                     )}
                 </CartesianChart>
             </View>
@@ -74,6 +79,12 @@ const styles = StyleSheet.create({
     stepContainer: {
         gap: 8,
         marginBottom: 8,
+    },
+    chart: {
+        
+        height: 300, 
+        backgroundColor: '#f0f0f0',
+        flex: 1,
     },
     reactLogo: {
         height: 190,
