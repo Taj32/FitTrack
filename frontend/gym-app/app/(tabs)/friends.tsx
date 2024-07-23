1c63d5e22ebfd9578ca1c06952adf5c796e83ec1
Tajvir Chahal <tajvirchahal87@gmail.com>
2024-07-22 23:15:32 -0400

    7/22 - user search bar work done

--- frontend/gym-app/app/(tabs)/friends.tsx
+++ frontend/gym-app/app/(tabs)/friends.tsx
@@ -1,7 +1,7 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, SafeAreaView, View, Text, ScrollView, Dimensions, FlatList, Alert } from 'react-native';
+import { StyleSheet, Image, Platform, SafeAreaView, View, Text, ScrollView, Dimensions, FlatList, Alert, Modal, ActivityIndicator, TextInput } from 'react-native';
 import { SvgUri } from 'react-native-svg';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useCallback, useEffect, useRef, useState } from 'react';
@@ -39,10 +39,10 @@ const FriendItem = React.memo(({ friend, onRemove, isEditMode }) => {
     });
 
     return (
-        <View 
+        <View
             style={styles.friendItemContainer}
             onLayout={(event) => {
-                const {width} = event.nativeEvent.layout;
+                const { width } = event.nativeEvent.layout;
                 setContainerWidth(width);
             }}
         >
@@ -76,13 +76,34 @@ const FriendItem = React.memo(({ friend, onRemove, isEditMode }) => {
     );
 });
 
+const UserItem = React.memo(({ user, onAdd }) => (
+    <TouchableOpacity onPress={() => onAdd(user.id)} style={styles.userItem}>
+        <Image
+            source={require('@/assets/images/average-user-sample.png')}
+            style={styles.userProfilePic}
+        />
+        <ThemedText style={styles.userName}>{user.name}</ThemedText>
+    </TouchableOpacity>
+));
+
 export default function FriendScreen() {
     const [friends, setFriends] = useState([]);
     const [isEditMode, setIsEditMode] = useState(false);
 
+    const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchKeyword, setSearchKeyword] = useState('');
+
+
+
     useEffect(() => {
         fetchFriends();
     }, []);
+
+    useEffect(() => {
+        console.log('Users state updated:', users);
+    }, [users]);
 
     const fetchFriends = async () => {
         try {
@@ -106,6 +127,35 @@ export default function FriendScreen() {
         }
     };
 
+    const fetchUsers = useCallback(async () => {
+        setIsLoading(true);
+        setSearchKeyword(''); // Reset search keyword
+
+        try {
+            const userToken = await AsyncStorage.getItem('userToken');
+            const response = await fetch(`${API_URL}/auth/getUsers`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${userToken}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch users');
+            }
+
+            const data = await response.json();
+            // The response is an array, so we can set it directly to the users state
+            setUsers(data);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            alert('Failed to fetch users. Please try again.');
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
+
     const removeFriend = useCallback(async (id) => {
         Alert.alert(
             "Confirm Deletion",
@@ -127,11 +177,11 @@ export default function FriendScreen() {
                                     'Authorization': `Bearer ${userToken}`
                                 }
                             });
-    
+
                             if (!response.ok) {
                                 throw new Error('Failed to remove friend');
                             }
-    
+
                             LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
                             setFriends(prevFriends => prevFriends.filter(friend => friend.id !== id));
                         } catch (error) {
@@ -145,6 +195,38 @@ export default function FriendScreen() {
             { cancelable: false }
         );
     }, []);
+
+    const handleSearch = useCallback(async (keyword) => {
+        setIsLoading(true);
+        console.log("keyword" + keyword);
+        try {
+            const userToken = await AsyncStorage.getItem('userToken');
+            console.log(`${API_URL}/auth/getUsers?keyword=${encodeURIComponent(keyword)}`);
+            const response = await fetch(`${API_URL}/auth/getusers?keyword=${encodeURIComponent(keyword)}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${userToken}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch users');
+            }
+
+            const data = await response.json();
+            console.log("data " + data);
+            setUsers(data);
+        } catch (error) {
+            console.error('Error searching users:', error);
+            alert('Failed to search users. Please try again.');
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
+
+
+
     const toggleEditMode = useCallback(() => {
         LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
         setIsEditMode(prev => !prev);
@@ -175,7 +257,10 @@ export default function FriendScreen() {
                     size={24}
                     color='#007AFF'
                     style={{ marginRight: 16 }}
-                    onPress={() => alert('plus pressed')}
+                    onPress={() => {
+                        setIsAddModalVisible(true);
+                        fetchUsers();
+                    }}
                 />
             </ThemedView>
 
@@ -200,6 +285,62 @@ export default function FriendScreen() {
                     <Text style={styles.defaultText}>Fitness is more fun with friends! Use the add button to cheer each other on and crush your goals together.</Text>
                 </View>
             )}
+
+            <Modal
+                visible={isAddModalVisible}
+                animationType="slide"
+                transparent={true}
+                onRequestClose={() => setIsAddModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+
+                    <View style={styles.modalContent}>
+                        <ThemedText style={styles.modalTitle}>Add Friend</ThemedText>
+                        <View style={styles.searchContainer}>
+                            <TextInput
+                                style={styles.searchInput}
+                                placeholder="Search users..."
+                                value={searchKeyword}
+                                onChangeText={(text) => {
+                                    setSearchKeyword(text);
+                                    if (text.length > 2) {
+                                        handleSearch(text);
+                                    } else if (text.length === 0) {
+                                        fetchUsers(); // Fetch all users when search is cleared
+                                    }
+                                }}
+                            />
+                        </View>
+                        {isLoading ? (
+                            <ActivityIndicator size="large" color="#0000ff" />
+                        ) : (
+                            <FlatList
+                                data={users}
+                                renderItem={({ item }) => (
+                                    <UserItem
+                                        user={item}
+                                        onAdd={(userId) => {
+                                            console.log(`Add friend with id: ${userId}`);
+                                            setIsAddModalVisible(false);
+                                        }}
+                                    />
+                                )}
+                                keyExtractor={(item) => item.id.toString()}
+                                ListEmptyComponent={<ThemedText>No users found</ThemedText>}
+                            />
+                        )}
+                        <TouchableOpacity
+                            style={styles.closeButton}
+                            onPress={() => {
+                                setIsAddModalVisible(false);
+                                setSearchKeyword(''); // Clear search when closing modal
+                            }}
+                        >
+                            <ThemedText style={styles.closeButtonText}>Close</ThemedText>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </SafeAreaView>
     );
 }
@@ -301,6 +442,73 @@ const styles = StyleSheet.create({
     },
     friendName: {
         fontSize: 17,
+    },
+
+
+
+
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        width: '90%',
+        maxHeight: '80%',
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15,
+    },
+    closeButton: {
+        marginTop: 20,
+        padding: 10,
+    },
+    closeButtonText: {
+        color: '#007AFF',
+        fontSize: 16,
+    },
+    userItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    userProfilePic: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+    },
+    userName: {
+        fontSize: 16,
+    },
+
+    searchContainer: {
+        width: '100%',
+        marginBottom: 15,
+    },
+    searchInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingHorizontal: 10,
     },
 
     // friendItem: {
